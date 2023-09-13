1d8090093034753e614eeb07e93536141e131129
엘비스 <144521987+Elbyss@users.noreply.github.com>
2023-09-13 16:47:01 +0900

    Create 문자와_문자열.cpp

---
+++ BOJ/문자와_문자열.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n";
+
+int main(void)
+{
+  cin.tie(0);
+  ios::sync_with_stdio(0);
+  string s;
+  int i;
+  cin >> s >> i;
+  cout << s[i - 1];
+}
