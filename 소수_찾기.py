3cc5fbbab76eacfa453caea57e49c03ec7abfaa1
Elbyss <elysiabyss@gmail.com>
2024-02-15 21:14:54 +0900

    solved.ac:class2 - 소수 찾기

---
+++ 소수_찾기.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('./input.txt', 'rt')
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    n = int(input())
+    ans = 0
+    numbers = list(map(int, input().split()))
+
+    for num in numbers:
+        if is_prime(num):
+            ans += 1
+
+    print(ans)
