78f3525fd4985793dcc108eca597c900db418215
Elbyss <elysiabyss@gmail.com>
2024-09-19 22:58:18 +0900

    BOJ:1문제 - sw 수열 구하기 (실3)

---
+++ Implement/SW_수열_구하기.py
@@ -0,0 +1,15 @@
+import sys
+#sys.stdin = open('./input.txt', 'r')
+input = sys.stdin.readline
+n = int(input())
+L, R = 1, n
+arr = []
+while L < R:
+    arr.extend([R, L])
+    L += 1
+    R -= 1
+
+if L == R:
+    arr.append(L)
+
+print(*arr)
