b1b8600ee56c70ad6a77e74acb1074d414430308
Elbyss <elysiabyss@gmail.com>
2024-10-03 15:44:06 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+#sys.stdin = open('./input.txt', 'r')
+input = sys.stdin.readline
+cmd = input().strip()
+if cmd == 'SONGDO':
+  print ('HIGHSCHOOL')
+elif cmd == 'CODE':
+  print ('MASTER')
+elif cmd == '2023':
+  print ('0611')
+elif cmd == 'ALGORITHM':
+  print ('CONTEST')
