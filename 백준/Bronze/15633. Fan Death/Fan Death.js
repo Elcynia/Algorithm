65b09ffc661211510a8cabc31b4b82e8cb1065d5
Elbyss <elysiabyss@gmail.com>
2024-11-12 19:34:17 +0900

    [Bronze III] Title: Fan Death, Time: 100 ms, Memory: 10028 KB -BaekjoonHub

---
+++ 백준/Bronze/15633. Fan Death/Fan Death.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const n = fs.readFileSync(0).toString().trim().split().map(Number);
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+        sum += i;
+    }
+}
+
+console.log (sum * 5 - 24)
