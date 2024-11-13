fd9eeb499fe331da2242b47a5208be9e20acf0d7
Elbyss <elysiabyss@gmail.com>
2024-11-13 18:28:09 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = +fs.readFileSync(0).toString().trim().split('\n');
+
+if (input === 1) console.log('12 1600');
+else if (input === 2) console.log('11 894');
+else if (input === 3) console.log('11 1327');
+else if (input === 4) console.log('10 1311');
+else if (input === 5) console.log('9 1004');
+else if (input === 6) console.log('9 1178');
+else if (input === 7) console.log('9 1357');
+else if (input === 8) console.log('8 837');
+else if (input === 9) console.log('7 1055');
+else if (input === 10) console.log('6 556');
+else if (input === 11) console.log('6 773');
