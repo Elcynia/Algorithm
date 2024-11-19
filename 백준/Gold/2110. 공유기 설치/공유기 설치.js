61408bbb0b5fb020c939a84de358d891a4c76865
Elbyss <elysiabyss@gmail.com>
2024-11-19 17:57:32 +0900

    [Gold IV] Title: 공유기 설치, Time: 256 ms, Memory: 36244 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [N, C] = input[0].split(' ').map(Number); // 집 개수, 공유기 개수
+const x = input
+  .slice(1)
+  .map(Number)
+  .sort((a, b) => a - b);
+let left = 1;
+let right = x.at(-1) - x[0];
+let result = 0;
+
+function wireless(dist) {
+  let cnt = 1;
+  let lastPosition = x[0];
+
+  for (let i = 1; i < N; i++) {
+    if (x[i] - lastPosition >= dist) {
+      cnt++;
+      lastPosition = x[i];
+      if (cnt >= C) return true;
+    }
+  }
+
+  return false;
+}
+
+while (left <= right) {
+  const mid = Math.floor((left + right) / 2);
+
+  if (wireless(mid)) {
+    result = mid;
+    left = mid + 1;
+  } else {
+    right = mid - 1;
+  }
+}
+
+console.log(result);
