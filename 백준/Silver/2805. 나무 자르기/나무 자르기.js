15a5a8036a6d2dc9b1a245be9270c5e3121effc0
Elbyss <elysiabyss@gmail.com>
2024-11-19 16:29:43 +0900

    [Silver II] Title: 나무 자르기, Time: 972 ms, Memory: 144692 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [N, M] = input[0].split(' ').map(Number); // 나무 수, 가져가려 하는 나무의 길이
+const trees = input[1].split(' ').map(Number);
+trees.sort((a, b) => a - b);
+let left = 0;
+let right = trees.at(-1);
+let result = 0;
+
+while (left <= right) {
+  let mid = Math.floor((left + right) / 2);
+  let sum = 0;
+
+  for (let tree of trees) {
+    if (tree > mid) {
+      sum += tree - mid;
+    }
+  }
+  if (sum >= M) {
+    result = mid;
+    left = mid + 1;
+  } else {
+    right = mid - 1;
+  }
+}
+
+console.log(result);
