8a4e093fd670a15accba210c3b1641bb3091c84e
Elbyss <elysiabyss@gmail.com>
2024-11-21 14:39:00 +0900

    [Bronze III] Title: 더하기 4, Time: 104 ms, Memory: 9972 KB -BaekjoonHub

---
+++ 백준/Bronze/11024. 더하기 4/더하기 4.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const N = +input[0];
+for (let i = 1; i <= N; i++) {
+  console.log(
+    input[i]
+      .split(' ')
+      .map(Number)
+      .reduce((acc, cur) => acc + cur)
+  );
+}
