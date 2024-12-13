40e7c6f630db6092630423dae2418470d6d4abff
Elbyss <elysiabyss@gmail.com>
2024-12-14 03:33:38 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 96 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+console.log(":fan::fan::fan:");
+console.log(`:fan::${input}::fan:`);
+console.log(":fan::fan::fan:");
