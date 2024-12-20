fbd571e32a85b9af9751ea0de3f80cde223e5bfc
Elbyss <elysiabyss@gmail.com>
2024-12-20 19:58:39 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const n = +fs.readFileSync(0).toString().trim();
-
-r = 0;
+let arr = new Array(n + 2).fill(-1);
+arr[0] = 0;
+arr[1] = 1;
 function fibonacci(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
-  r += n;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  if (arr[n] !== -1) return arr[n];
+  arr[n] = fibonacci(n - 1) + fibonacci(n - 2);
+  return fibonacci(n);
 }
-
 console.log(fibonacci(n));
