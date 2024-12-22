792b935eeec4a1f0a08aa3df6c5cc66d7a60fed5
Elbyss <elysiabyss@gmail.com>
2024-12-22 22:27:38 +0900

    [Silver III] Title: 모든 순열, Time: 1672 ms, Memory: 13240 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const N = +fs.readFileSync(0).toString().trim();
+let result = [];
+let checked = new Array(N).fill(false);
+
+function permutation(level) {
+  if (level === N) {
+    console.log(result.join(' '));
+
+    return;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (checked[i]) continue;
+    result.push(i);
+    checked[i] = true;
+    permutation(level + 1);
+    checked[i] = false;
+    result.pop();
+  }
+}
+
+permutation(0);
