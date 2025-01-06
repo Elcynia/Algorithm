93e64317dc8fcdf9dab08e59e086ec502282943d
Elbyss <elysiabyss@gmail.com>
2025-01-06 19:23:05 +0900

    [Gold IV] Title: LCS 2, Time: 204 ms, Memory: 24812 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.js
+++ 백준/Gold/9252. LCS 2/LCS 2.js
@@ -25,3 +25,12 @@ for (let i = arr1.length, j = arr2.length; i > 0 && j > 0; ) {
 }
 console.log(dp[arr1.length][arr2.length]);
 console.log(lcsStr);
+
+/*
+?   LCS 문제에서 문자열을 추적하는 출력이 추가된 케이스.
+*  시간 복잡도 : O(MN)
+두 문자가 같은 경우, 문자를 lcsStr 앞에 추가한 뒤  위/왼쪽 모두 이전 위치로 이동한다 (i--, j--)
+왼쪽 보다 위쪽 값이 더 큰 경우(dp[i-1][j] > dp[i][j-1]), 위쪽으로 이동한다 (i--)
+위쪽 보다 왼쪽 값이 더 크거나 같은 경우 (dp[i-1][j] <= dp[i][j-1]) 왼쪽으로 이동한다 (j--)
+P(6,6) → K(5,6) → A(3,5) → C(2,4) = "ACAK"
+*/
