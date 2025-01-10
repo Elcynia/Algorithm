d982af660a77a3d46098c9f7622afaff168fd8a0
Elbyss <elysiabyss@gmail.com>
2025-01-10 23:18:41 +0900

    [Silver IV] Title: 베스트셀러, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.js
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.js
@@ -12,3 +12,10 @@ for (let i = 1; i <= N; i++) {
 const maxCnt = Math.max(...Object.values(bookCnt));
 const result = Object.keys(bookCnt).filter((book) => bookCnt[book] === maxCnt);
 console.log(result.sort()[0]);
+
+/**
+ * 책이 등장할 때마다 해당 책의 카운트 +1 증가
+ * 가장 많이 등장한 책 제목을 maxCnt에 저장
+ * 사전 순으로 정렬하여 가장 앞에 있는 제목 출력
+ *
+ */
