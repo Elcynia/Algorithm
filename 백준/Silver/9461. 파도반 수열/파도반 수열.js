b32e6fdea56894e7062d076bb3b99eb5066bdce5
Elbyss <elysiabyss@gmail.com>
2025-01-12 19:01:26 +0900

    [Silver III] Title: 파도반 수열, Time: 100 ms, Memory: 9844 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -12,3 +12,9 @@ for (let i = 1; i <= T; i++) {
   const N = +input[i];
   console.log(dp[N]);
 }
+
+/**
+ * dp[N] = 5칸 전의 값과 1칸 전의 값의 합
+ * P(6) = 3 = P(1) + P(5)
+ * P(12) = 16 = P(4) + P(12)
+ */
