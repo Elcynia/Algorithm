b6c6b664f2ad7db82b5a67e547e92e97275c9b51
Elbyss <elysiabyss@gmail.com>
2025-01-13 13:52:09 +0900

    [Silver II] Title: 제곱수의 합, Time: 268 ms, Memory: 12764 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 const N = +fs.readFileSync(0).toString().trim();
-const dp = new Array(N + 1).fill(0);
-dp[1] = dp[4] = 1;
-dp[2] = 2;
-dp[3] = 3;
-
-for (let i = 5; i <= N; i++) {
+let dp = [0];
+for (let i = 1; i <= N; i++) {
   dp[i] = i;
   for (let j = 1; j * j <= i; j++) {
     dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
@@ -13,3 +9,8 @@ for (let i = 5; i <= N; i++) {
 }
 
 console.log(dp[N]);
+
+/**
+ * dp[i]: i^2의 합
+ * i에서 j^2을 뺀 수(i - j*j)의 최소 제곱수 개수에 1을 더한 값과 현재 dp[i]값을 비교
+ */
