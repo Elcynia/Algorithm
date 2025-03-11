1cf0d2ae221d159c1dec7965922d29fd7b980741
Elbyss <elysiabyss@gmail.com>
2025-03-11 16:27:07 +0900

    [Silver I] Title: 부분수열의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -2,20 +2,13 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 S = list(map(int, input().strip().split(' ')))
-mx = sum(S)
-possible_sums = set()
+result = 0
+S.sort()
 
-def generate_sums(idx, subset_sum):
-    if idx == N:
-        possible_sums.add(subset_sum)
-        return
-    
-    generate_sums(idx + 1, subset_sum + S[idx])
-    generate_sums(idx + 1, subset_sum)
+for i in S:
+  if result + 1 < i:
+    break
+  result += i
 
-generate_sums(0, 0)
+print(result + 1)
 
-for i in range(1, mx + 2):
-    if i not in possible_sums:
-        print(i)
-        break
