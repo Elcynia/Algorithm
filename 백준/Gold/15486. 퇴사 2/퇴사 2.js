9872f7a3bd39df740de3e3d8060f9322fcf1f9c5
Elcyn <elysiabyss@gmail.com>
2025-04-25 14:58:49 +0900

    [Gold V] Title: 퇴사 2, Time: 1556 ms, Memory: 168716 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const N = +input.shift();
+const dp = Array(N + 1).fill(0);
+
+for (let i = N - 1; i >= 0; i--) {
+  const [T, P] = input[i].split(' ').map(Number);
+  if (i + T <= N) {
+    dp[i] = Math.max(dp[i + 1], dp[i + T] + P);
+  } else {
+    dp[i] = dp[i + 1];
+  }
+}
+
+console.log(dp[0]);
