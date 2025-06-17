2c6e1e680f67a4fd7646ce391c75d2fc3a9b9699
Elcynia <elysiabyss@gmail.com>
2025-06-17 15:18:52 +0900

    [Bronze IV] Title: 과목선택, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

--- 백준/Bronze/11948. 과목선택/과목선택.js
+++ 백준/Bronze/11948. 과목선택/과목선택.js
@@ -1,5 +1,2 @@
-const fs = require('fs');
-const [a, b, c, d, e, f] = fs.readFileSync(0).toString().trim().split('\n').map(Number);
-const cal1 = Math.min(a, b, c, d);
-const cal2 = Math.min(e, f);
-console.log(a + b + c + d + e + f - cal1 - cal2);
+const [a, b, c, d, e, f] = require('fs').readFileSync(0).toString().trim().split('\n').map(Number);
+console.log(a + b + c + d + e + f - Math.min(a, b, c, d) - Math.min(e, f));
