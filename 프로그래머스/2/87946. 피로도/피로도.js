578dd2c4a7be18affb5957da8a077e6d935475fe
Elcynia <elysiabyss@gmail.com>
2025-11-03 15:35:35 +0900

    [level 2] Title: 피로도, Time: 6.56 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,19 @@
+function solution(k, dungeons) {
+  let answer = -1;
+  const visited = Array(dungeons.length).fill(false);
+
+  function dfs(currentK, count) {
+    answer = Math.max(answer, count);
+    for (let i = 0; i < dungeons.length; i++) {
+      if (!visited[i] && currentK >= dungeons[i][0]) {
+        visited[i] = true;
+        dfs(currentK - dungeons[i][1], count + 1);
+        visited[i] = false;
+      }
+    }
+  }
+
+  dfs(k, 0);
+
+  return answer;
+}
